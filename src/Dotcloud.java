5f1b09c793590ad2169634c1e6f9db8ebd820fbf
erictapen <justin.humm@posteo.de>
2015-08-13 18:17:06 +0200

    begin on svg export

--- src/Dotcloud.java
+++ src/Dotcloud.java
@@ -1,5 +1,9 @@
 import java.awt.image.BufferedImage;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Vector;
 
 
@@ -42,7 +46,48 @@ public class Dotcloud {
 				this.dots.add(dot);
 				amount--;
 			}
+		}	
+	}
+	
+	public void exportSVG(String filename, boolean tspEnabled) {
+		
+		Locale.setDefault(Locale.ENGLISH);
+		DecimalFormat df = new DecimalFormat("#.########");
+		try{
+			FileWriter writer = new FileWriter(filename);
+			String append = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
+					"<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" "
+					+ "\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n" + 
+					"\n" + 
+					"<svg xmlns=\"http://www.w3.org/2000/svg\"\n" + 
+					"     xmlns:xlink=\"http://www.w3.org/1999/xlink\" "
+					+ "xmlns:ev=\"http://www.w3.org/2001/xml-events\"\n" + 
+					"     version=\"1.1\" baseProfile=\"full\"\n" + 
+					"     width=\"%widthpx\" height=\"%heightpx\"\n" + 
+					"     viewBox=\"%cornerx %cornery %width %height\">\n" + 
+					"\n";
+			append = append.replaceAll("%width", df.format(width*scale));
+			append = append.replaceAll("%height", df.format(height*scale));
+			append = append.replaceAll("%cornerx", df.format(posxmin*scale));
+			append = append.replaceAll("%cornery", df.format(posymin*scale));
+			
+			writer.append(append);
+			
+			writer.append(
+					"\\n\" + \n" + 
+					"					\"</svg>"
+					);
+
+			writer.flush();
+			writer.close();
 		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void solveTSP(int iterations) {
 		
 	}
 	
